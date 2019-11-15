385da259ab4a24d99a055000e4fa98c5e61643f7
Coen Jacobs <coenjacobs@gmail.com>
2019-11-15 14:06:45 +0100

    Migration to setup relation between keys and products

--- src/Database/Installer.php
+++ src/Database/Installer.php
@@ -4,6 +4,7 @@ namespace Lapisense\Database;
 
 use Lapisense\Database\Migrations\CreateActivationsTable;
 use Lapisense\Database\Migrations\CreateKeysTable;
+use Lapisense\Database\Migrations\CreateProductsKeysPivotTable;
 use Lapisense\Dependencies\CoenJacobs\Migrator\Handler;
 use Lapisense\Dependencies\CoenJacobs\Migrator\Loggers\DatabaseLogger;
 use Lapisense\Dependencies\CoenJacobs\Migrator\Workers\WpdbWorker;
@@ -19,6 +20,7 @@ class Installer
 
         $handler->add('lapisense', CreateKeysTable::class);
         $handler->add('lapisense', CreateActivationsTable::class);
+        $handler->add('lapisense', CreateProductsKeysPivotTable::class);
 
         $handler->up('lapisense');
     }
---
+++ src/Database/Migrations/CreateProductsKeysPivotTable.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Lapisense\Database\Migrations;
+
+use Lapisense\Dependencies\CoenJacobs\Migrator\Migrations\BaseMigration;
+
+class CreateProductsKeysPivotTable extends BaseMigration
+{
+    public static function id()
+    {
+        return 'lapisense-3-products-keys-pivot-table';
+    }
+
+    public function up()
+    {
+        $tableName = $this->worker->getPrefix() . 'lapisense_products_keys';
+        $productsTable = $this->worker->getPrefix() . 'posts';
+        $keysTable = $this->worker->getPrefix() . 'lapisense_keys';
+
+        $query = "CREATE TABLE $tableName (
+    product_id bigint UNSIGNED NOT NULL,
+    key_id bigint UNSIGNED NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES $productsTable(ID),
+    FOREIGN KEY (key_id) REFERENCES $keysTable(id) )";
+
+        $this->worker->query($query);
+    }
+
+    public function down()
+    {
+        $tableName = $this->worker->getPrefix() . 'lapisense_products_keys';
+
+        $query = "DROP TABLE $tableName";
+        $this->worker->query($query);
+    }
+}
