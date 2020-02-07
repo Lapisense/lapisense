8301e61fbf8a66111ee932dc2ba48a2ee6e42f75
Coen Jacobs <coenjacobs@gmail.com>
2020-02-07 13:19:47 +0100

    Keys and Activation pages show their respective list table

--- src/Admin/Pages/KeysPage.php
+++ src/Admin/Pages/KeysPage.php
@@ -2,10 +2,22 @@
 
 namespace Lapisense\Admin\Pages;
 
+use Lapisense\Admin\Tables\KeysTable;
+use Lapisense\Database\Repositories\KeysRepository;
+
 class KeysPage extends Page
 {
     public function output()
     {
-        echo 'This is the license keys page';
+        $table = new KeysTable();
+        $table->setRepository(new KeysRepository());
+        $table->prepare_items();
+
+        ?>
+        <div class="wrap">
+            <h2>License Keys</h2>
+            <?php $table->display(); ?>
+        </div>
+        <?php
     }
 }
